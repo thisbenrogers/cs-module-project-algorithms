6a95395fa635b26764f69b107c7c0f78fc528103
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-08-13 22:04:04 -0500

    Implements sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,36 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+from collections import deque
+
+def sliding_window_max(nums, k):
+    # * using deque() solution
+    # *     deque() stores indexes of values in each window, maintining 
+    # *     a decreasing order so that the max of each window can easily
+    # *     be found at the front of the 'qu'
+    # * 
+    # *     the while loops are removing the smaller items from the window view
+    # * 
+
+    n = len(nums)
+    qu = deque()
+    arr = [] * (len(nums) - k)
+
+    for i in range(k):
+        while qu and nums[i] >= nums[qu[-1]]:
+            qu.pop()
+        qu.append(i)
+
+    for i in range(k, n):
+        arr.append(nums[qu[0]])
+        while qu and qu[0] <= (i - k):
+            qu.popleft()
+        while qu and nums[i] >= nums[qu[-1]]:
+            qu.pop()
+        qu.append(i)
+    arr.append(nums[qu[0]])
+    return arr
 
 
 if __name__ == '__main__':
